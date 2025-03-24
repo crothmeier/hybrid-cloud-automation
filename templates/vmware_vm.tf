7817816e480d7aaf53564604bad9489536aeaefc
crothmeier <42760817+crothmeier@users.noreply.github.com>
2025-03-24 03:12:37 -0400

    Create vmware_vm.tf

---
+++ templates/vmware_vm.tf
@@ -0,0 +1,46 @@
+provider "vsphere" {
+  # vSphere connection configuration (fill in or use env variables)
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "datastore1"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "Resources"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "example-vm"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 2048
+  guest_id = "otherGuest"  # Placeholder OS type; use a valid guest_id for real use
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = "vmxnet3"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
